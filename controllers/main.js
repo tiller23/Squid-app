384572daee355b5b77e0f0dc56eea8197efc8968
tiller23 <tylervaughndavis@gmail.com>
2023-06-06 13:23:28 -0500

    hmm

--- controllers/main.js
+++ controllers/main.js
@@ -6,5 +6,5 @@ module.exports.alive = function(req, res){
 };
 
 module.exports.home = function(req, res){
-    return res.render(path.join(__dirname, '../', '/index.html')); 
+    return res.sendFile(path.join(__dirname, '../', '/index.html')); 
 };
