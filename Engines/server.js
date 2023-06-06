51e7c8e6e3a114569f3c4af0d29e67f98add93a1
tiller23 <tylervaughndavis@gmail.com>
2023-06-06 12:31:01 -0500

    trying

--- Engines/server.js
+++ Engines/server.js
@@ -11,7 +11,7 @@ Server.init = function(settings){
    Server.app = express();
    Server.app.use(sessions(settings));
    Server.app.use(cookieParser());
-   Server.app.use(express.static(path.join(__dirname,'./')));
+   Server.app.use(express.static(path.join(__dirname,'./projects')));
    Server.app.use(function(req, res, next){
       console.log({
          message: 'incoming request',
--- routes.js
+++ routes.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 //All of the routes to diffrent things, implement the controls set 
 //through different objects that pull from the controllers folder
-server.app.use(express.static(path.join(__dirname, './projects')));
 server.app.get('/alive', alive);
 server.app.get('/', home);
 server.app.get('/logout', logout);
