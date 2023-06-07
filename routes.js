1798cb043ce2120ab507386562d74d07277dd5d9
tiller23 <tylervaughndavis@gmail.com>
2023-06-07 11:00:36 -0500

    help me

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const path = require('path');
 //All of the routes to diffrent things, implement the controls set 
 //through different objects that pull from the controllers folder
 server.app.get('/alive', alive);
-server.app.get('/app1', home);
+server.app.get('/', home);
 server.app.get('/logout', logout);
 server.app.post('/register', register);
 server.app.post('/login', login);
