3e72dfc1e6708c4c1b0dfb6dd271250a1d7fc149
tiller23 <tylervaughndavis@gmail.com>
2023-06-07 12:52:58 -0500

    test

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const server = require('./Engines/server');
 const {alive, home} = require('./controllers/main');
 const {logout, login} = require('./controllers/user');
-const {register, newsReg} = require('./controllers/registration');
+const {register, newsReg, serve} = require('./controllers/registration');
 const path = require('path');
 
 //All of the routes to diffrent things, implement the controls set 
@@ -9,6 +9,7 @@ const path = require('path');
 server.app.get('/alive', alive);
 server.app.get('/', home);
 server.app.get('/logout', logout);
+server.app.get('/register', serve);
 server.app.post('/register', register);
 server.app.post('/login', login);
 server.app.post('/newsLetter', newsReg);
