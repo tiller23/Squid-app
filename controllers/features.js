5c0147423ab5d468aa42a02eeb3b7597fc8585d4
tiller23 <tylervaughndavis@gmail.com>
2023-06-08 16:13:29 -0500

    link fixiing

--- controllers/features.js
+++ controllers/features.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+module.exports.success = function(req, res){
+    res.sendFIle(path.join(__dirname, '../', './loginSucess.html'));
+}
+
 module.exports.shop = function(req, res){
     return res.sendFile(path.join(__dirname, '../', './shopping.html'));
 };
