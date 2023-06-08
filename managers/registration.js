e0d6df4456269b77d344dfa200a2f277a4063780
tiller23 <tylervaughndavis@gmail.com>
2023-06-08 13:00:56 -0500

    error logging

--- managers/registration.js
+++ managers/registration.js
@@ -4,12 +4,18 @@ const path = require('path');
 module.exports.register = async function(data){
     //This allows the sql connection to make sure that the credentials inserted are not already in the system
     let result = await mysql.connection.query(`SELECT * FROM accounts WHERE username = '${data.userName}' AND password  = '${data.password}'`);
+    if (err){
+        console.log(err);
+    };
     if(Object.keys(result).length > 0){
         return path.join(__dirname, '../', './regFail.html');
     }
     
     //After checking if the user is already in the system, inserts their credentials into the database
     await mysql.connection.query(`INSERT INTO accounts (email, username, password) VALUES ('${data.email}', '${data.userName}', '${data.password}')`);
+    if(err){
+        console.log(err);
+    };
     return path.join(__dirname, '../', './regSucess.html');
 };
 
