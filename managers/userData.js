05abe9684376e073a7e852ba4903a7c11f6d0a0d
tiller23 <tylervaughndavis@gmail.com>
2023-07-13 13:01:28 -0500

    hello

--- managers/userData.js
+++ managers/userData.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports.login = async function(data){
     let result = await mysql.connection.query(`SELECT * FROM accounts WHERE username = '${data.userName}' AND password = '${data.password}'`);
-    console.log(result);
+    console.log(result.values);
     if(Object.keys(result).length > 0){
         return path.join(__dirname, '../', './loginSucess.html');
     }else{
