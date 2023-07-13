f8d8f4f6222590ffefe1f6be2ed78a7df824b631
tiller23 <tylervaughndavis@gmail.com>
2023-07-13 12:59:51 -0500

    commit

--- empty.js
+++ empty.js
@@ -1,5 +1,12 @@
 const axios = require('axios');
 axios.post('https://app.mysquid.io/api/v1/track', {
     project: "6480ea9e2cfb668a330b87a5",
-    event: "click"
+    event: "identify",
+    label: "recurring friend",
+    context: {
+        traits: {
+            name: "tyler",
+            email: "keyboard@swim.net"
+        }
+    }
 })
