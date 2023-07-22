2abfa134306a8fdd13cd313df1fa50c9aec03b3f
tiller23 <tylervaughndavis@gmail.com>
2023-07-22 10:54:11 -0500

    commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //All the requires that brings in the the necessary files to give everything proper scope.
 const Config = require('./config/development');
 const mysql = require('./Engines/mysql');
-//const Server = require('./Engines/server');
+const Server = require('./Engines/server');
 
 //the configurations for the server
 Server.init(Config.server);
-mysql.init(Config.mysql);
+//mysql.init(Config.mysql);
 
-require('./routes');
+//require('./routes');
