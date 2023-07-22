1efef309754483c7cb758bf3344c729f0fa714c8
tiller23 <tylervaughndavis@gmail.com>
2023-07-22 11:02:49 -0500

    o

--- Engines/server.js
+++ Engines/server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const path = require('path');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const passport = require('passport');
 const Server = {};
 
 
 Server.init = function(settings){
-   /*Server.app = express();
+   Server.app = express();
    Server.app.use(sessions(settings));
    Server.app.use(cookieParser());
    Server.app.use(bodyParser.urlencoded({extended: false}));
@@ -27,7 +27,7 @@ Server.init = function(settings){
    Server.app.engine('html', require('ejs').renderFile);
    Server.app.set("view engine", "html");
    Server.app.set('views', path.join(__dirname, '../views'));
-   Server.app.listen(3001, () =>{        console.log("Server running on port 3001");    });*/
+   Server.app.listen(3001, () =>{        console.log("Server running on port 3001");    });
 }
 
 module.exports = Server;
